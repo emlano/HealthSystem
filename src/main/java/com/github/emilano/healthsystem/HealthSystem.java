b1453d729749b17f32865996ea56bf59a504cbee
emilano <emlano.git@gmail.com>
2024-04-25 18:12:00 +0530

    init: Set up project dir

---
+++ src/main/java/com/github/emilano/healthsystem/HealthSystem.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.emilano.healthsystem;
+
+/**
+ *
+ * @author emilano
+ */
+public class HealthSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");    }
+}
---
+++ src/main/java/com/github/emilano/healthsystem/dao/MedicalRecordDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.github.emilano.healthsystem.dao;
+
+import com.github.emilano.healthsystem.entity.*;
+
+/**
+ *
+ * @author emilano
+ */
+public class MedicalRecordDAO {
+    
+}
