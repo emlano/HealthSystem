abf2308394e8ed88f1326164908cb7782c15f0ff
emilano <emlano.git@gmail.com>
2024-04-28 19:17:57 +0530

    feat: Add BillingResource class

---
+++ src/main/java/com/github/emilano/healthsystem/resources/BillingResource.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.github.emilano.healthsystem.resources;
+
+import com.github.emilano.healthsystem.dao.BillingDAO;
+import com.github.emilano.healthsystem.entity.Billing;
+import java.util.Collection;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author emilano
+ */
+@Path("/billing")
+public class BillingResource {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Billing> getAllBillings() {
+        return BillingDAO.getAllBilling();
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Billing getBilling(@PathParam("id") long id) {
+        return BillingDAO.getBilling(id);
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void postBilling(Billing billing) {
+        BillingDAO.addBilling(billing);
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putBilling(@PathParam("id") long id, Billing updated) {
+        Billing billing = BillingDAO.getBilling(id);
+        updated.setId(id);
+        BillingDAO.updateBilling(updated);
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public void deleteBilling(@PathParam("id") long id) {
+        BillingDAO.deleteBilling(id);
+    }
+}
